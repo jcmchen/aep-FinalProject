ffdd36f298ad020db453a1df3c10230f111e2ed5
Jeremy Chen <jeremychen2@gmail.com>
2023-04-18 11:04:04 -0700

    Addition Test

--- src/test/java/edu/berkeley/aep/CalculatorTest.java
+++ src/test/java/edu/berkeley/aep/CalculatorTest.java
@@ -12,14 +12,14 @@ public class CalculatorTest {
         Calculator cal = new Calculator();
         assertEquals(2, cal.addition(1,1));
     }
-    @Test
-    public void twoMinusOneShouldBeONE(){
-        Calculator cal = new Calculator();
-        assertEquals(1, cal.subtraction(2,1));
-    }
-    @Test
-    public void twoMultipliedByThreeShouldBeSIX(){
-        Calculator cal = new Calculator();
-        assertEquals(6, cal.multiplication(2,3));
-    }
+//    @Test
+//    public void twoMinusOneShouldBeONE(){
+//        Calculator cal = new Calculator();
+//        assertEquals(1, cal.subtraction(2,1));
+//    }
+//    @Test
+//    public void twoMultipliedByThreeShouldBeSIX(){
+//        Calculator cal = new Calculator();
+//        assertEquals(6, cal.multiplication(2,3));
+//    }
 }
